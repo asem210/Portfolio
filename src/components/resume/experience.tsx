a9c8abf9279f2f8b3911f0a3a0daba028116dadd
asem210 <gustavoadrianpazos@gmail.com>
2024-12-29 21:16:16 -0500

    experience update

--- src/components/resume/experience.tsx
+++ src/components/resume/experience.tsx
@@ -1,21 +1,36 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+import ResumeCard from "./card";
 
 export const Experiences: React.FC<{}> = () => {
   return (
-    <div className="flex flex-row rounded-lg bg-gradient-to-t from-[#314053] to-thirdmisc group mt-14 h-fit justify-center items-center py-5 sm:px-10 px-2">
-      <div className="w-full md:text-[36px] sm:text-[20px] text-[12px] flex flex-col  font-poppins md:text-justify text-left px-2">
-        Actualmente no cuento con experiencia profesional, pero puedes ser el
-        primero en darme la oportunidad de demostrar mis capacidades.
-      </div>
-      <div className="w-full flex items-center justify-center ">
-        <div>
-          <img
-            className="sm:w-[300px] sm:h-[300px] w-[200px] h-[250px] md:h-[500px] md:w-[500px]"
-            src="https://drive.google.com/uc?export=view&id=1qQSqPyvF-JDS5L-ZVFeMBHtDyPy8JXNe"
-            alt="src"
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex flex-col gap-10 lg:gap-20"
+    >
+      {/* Parte única: Experiencia laboral */}
+      <div>
+        <div className="py-6 lg:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">
+            2024-Actualidad
+          </p>
+          <h2 className="text-3xl md:text-4xl font-bold">
+            Experiencia Laboral
+          </h2>
+        </div>
+        <div className="mt-6 lg:mt-14 w-full border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Desarrollador Trainee"
+            subTitle="Moventi - (2024 - Actualidad)"
+            result="En progreso"
+            des="Mantuve software no crítico para apoyar las operaciones del sistema. Colaboré con los clientes para comprender sus requisitos y asegurar que las soluciones cumplieran con los objetivos comerciales. Trabajé con el equipo para proporcionar documentación técnica."
+            linkState={false}
+            link=""
           />
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
